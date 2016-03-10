df77241415aec21b3310a2fdf36217e828a8c6b9
Mario Acchione <maacchione@gmail.com>
2016-03-09 20:04:07 -0500

    Initial Commit

---
+++ Duplicate Word Counter/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Duplicate Word Counter
+//
+//  Created by Mario Acchione on 2016-03-09.
+//  Copyright © 2016 Mario Acchione. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
