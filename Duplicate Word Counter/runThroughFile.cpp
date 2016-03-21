5d41942440d0a83298a910913721faa0bf56b9cc
Mario Acchione <maacchione@gmail.com>
2016-03-21 00:13:11 -0400

    first commit

---
+++ Duplicate Word Counter/runThroughFile.cpp
@@ -0,0 +1,9 @@
+//
+//  runThroughFile.cpp
+//  Duplicate Word Counter
+//
+//  Created by Mario Acchione on 2016-03-20.
+//  Copyright © 2016 Mario Acchione. All rights reserved.
+//
+
+#include "runThroughFile.hpp"
---
+++ Duplicate Word Counter/runThroughFile.hpp
@@ -0,0 +1,14 @@
+//
+//  runThroughFile.hpp
+//  Duplicate Word Counter
+//
+//  Created by Mario Acchione on 2016-03-20.
+//  Copyright © 2016 Mario Acchione. All rights reserved.
+//
+
+#ifndef runThroughFile_hpp
+#define runThroughFile_hpp
+
+#include <stdio.h>
+
+#endif /* runThroughFile_hpp */
